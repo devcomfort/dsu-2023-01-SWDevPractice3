37fdb98b19e66ddb145973a09a506c201d74c7e2
devcomfort <kimdonghyun026@gmail.com>
2023-06-12 08:34:33 +0900

    comment: todo 추가

--- backend/src/services/losts.ts
+++ backend/src/services/losts.ts
@@ -2,6 +2,8 @@ import { PrismaClient, Prisma } from "@prisma/client";
 
 const client = new PrismaClient();
 
+// TODO: 중복 검사 코드 추가
+
 const insertLost = async (post: Prisma.lostsCreateInput) => {
   if (
     !(await client.losts.findFirst({

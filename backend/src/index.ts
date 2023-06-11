e003b554a2d532e78146196e4a1d11b682c2aa4b
devcomfort <kimdonghyun026@gmail.com>
2023-06-12 08:32:40 +0900

    feat : 서버 코드 추가

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import Koa from "koa";
+import Router from "koa-router";
+import Helmet from "koa-helmet";
+import Logger from "koa-logger";
+import BodyParser from "koa-bodyparser";
+
+import RefreshLostsRouter from "./controllers/refresh-losts.js";
+
+const app = new Koa();
+const router = new Router();
+
+router.use(RefreshLostsRouter.routes());
+
+app.use(router.routes());
+app.use(Logger());
+app.use(Helmet());
+app.use(BodyParser());
+
+app.listen(8000, () => {
+  console.log("8000번 포트로 서버가 시작되었습니다!");
+});

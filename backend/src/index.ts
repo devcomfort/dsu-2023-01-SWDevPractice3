07d9a8f800a9a45ab81366f890aae249a890eda9
devcomfort <kimdonghyun026@gmail.com>
2023-06-18 19:38:07 +0900

    feat : 구독자 관련 라우터 추가

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,18 +6,20 @@ import BodyParser from "koa-bodyparser";
 
 import RefreshLostsRouter from "./controllers/refresh-losts.js";
 import QueryLostsRouter from "./controllers/query-losts.js";
+import SubscriptionRouter from "./controllers/subscription.js";
 
 const app = new Koa();
 const router = new Router();
 
 router.use(RefreshLostsRouter.routes());
 router.use(QueryLostsRouter.routes());
+router.use(SubscriptionRouter.routes());
 
 app.use(router.routes());
 app.use(Logger());
 app.use(Helmet());
 app.use(BodyParser());
 
-app.listen(8000, () => {
-  console.log("8000번 포트로 서버가 시작되었습니다!");
+app.listen(80, () => {
+  console.log("80번 포트로 서버가 시작되었습니다!");
 });
